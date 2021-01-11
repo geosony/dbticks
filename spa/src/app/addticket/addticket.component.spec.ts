77930e463a722c144bce661b301040b97f41e5f9
Sony George <sony@thinkberries.com>
2021-01-11 16:26:50 +0530

    Removed all test cases for time being

--- spa/src/app/addticket/addticket.component.spec.ts
+++ spa/src/app/addticket/addticket.component.spec.ts
@@ -3,23 +3,4 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddticketComponent } from './addticket.component';
 
 describe('AddticketComponent', () => {
-  let component: AddticketComponent;
-  let fixture: ComponentFixture<AddticketComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddticketComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddticketComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- spa/src/app/tickets/tickets.component.spec.ts
+++ spa/src/app/tickets/tickets.component.spec.ts
@@ -2,24 +2,4 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketsComponent } from './tickets.component';
 
-describe('TicketsComponent', () => {
-  let component: TicketsComponent;
-  let fixture: ComponentFixture<TicketsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TicketsComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TicketsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+describe('TicketsComponent', () => {});
