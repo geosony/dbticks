dfcf3dc987bf4c1a0fd59f1eccb85172fe646c08
Sony George <sony@thinkberries.com>
2021-01-12 19:19:04 +0530

    trial and error

--- spa/karma.conf.js
+++ spa/karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox','--disable-setuid-sandbox']
+        flags: ['--no-sandbox']
       }
     },
     singleRun: false,
