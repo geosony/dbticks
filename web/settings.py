e1d66d5056dd4ff033a6a73e23af4a6e2e28f825
Sony George <sony@thinkberries.com>
2021-01-13 11:22:55 +0530

    Changed travis and web .ini

--- web/settings.py
+++ web/settings.py
@@ -16,8 +16,8 @@ tornado.options.parse_command_line()
 ilog.info("Starting db")
 
 try:
-    _mongo_user = db_settings['mongo_user'] if db_settings['mongo_host'] else ''
-    _mongo_pass = db_settings['mongo_pass'] if db_settings['mongo_host'] else ''
+    _mongo_user = db_settings['mongo_user'] if db_settings['mongo_user'] else ''
+    _mongo_pass = db_settings['mongo_pass'] if db_settings['mongo_pass'] else ''
     _mongo_host = db_settings['mongo_host'] if db_settings['mongo_host'] else 'localhost'
     _mongo_port = db_settings['mongo_port'] if db_settings['mongo_port'] else 27017
     _mongo_connect_params = db_settings['mongo_connect_params'] if db_settings['mongo_connect_params'] else ''
@@ -25,6 +25,9 @@ try:
     if _mongo_user:
         
         _conn_uri = 'mongodb://{user}:{pswd}@{host}:{port}{connect_params}'.format(user=_mongo_user, pswd=_mongo_pass, host=_mongo_host, port=_mongo_port, connect_params=_mongo_connect_params)
+        
+        ilog.info("CONNECTION_URI: {}".format(_conn_uri))
+
         _client = motor.motor_tornado.MotorClient(_mongo_host, _mongo_port)
     else:
         _client = motor.motor_tornado.MotorClient(_mongo_host, _mongo_port).dbtickets
