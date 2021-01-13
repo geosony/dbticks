435581dd3259273a71acb623b728d1214576b497
Sony George <sony@thinkberries.com>
2021-01-13 11:53:34 +0530

    changed the web mongo connection settings

--- web/settings.py
+++ web/settings.py
@@ -15,27 +15,31 @@ tornado.options.parse_command_line()
 
 ilog.info("Starting db")
 
+_mongo_user = db_settings['mongo_user'] if db_settings['mongo_user'] else ''
+_mongo_pass = db_settings['mongo_pass'] if db_settings['mongo_pass'] else ''
+_mongo_host = db_settings['mongo_host'] if db_settings['mongo_host'] else 'localhost'
+_mongo_port = db_settings['mongo_port'] if db_settings['mongo_port'] else 27017
+_mongo_connect_params = db_settings['mongo_connect_params'] if db_settings['mongo_connect_params'] else ''
+_conn_uri = ''
+
+if _mongo_user:
+    
+    _conn_uri = 'mongodb://{user}:{pswd}@{host}:{port}{connect_params}'.format(user=_mongo_user, pswd=_mongo_pass, host=_mongo_host, port=_mongo_port, connect_params=_mongo_connect_params)
+    
+    ilog.info("CONNECTION_URI: {}".format(_conn_uri))
+
 try:
-    _mongo_user = db_settings['mongo_user'] if db_settings['mongo_user'] else ''
-    _mongo_pass = db_settings['mongo_pass'] if db_settings['mongo_pass'] else ''
-    _mongo_host = db_settings['mongo_host'] if db_settings['mongo_host'] else 'localhost'
-    _mongo_port = db_settings['mongo_port'] if db_settings['mongo_port'] else 27017
-    _mongo_connect_params = db_settings['mongo_connect_params'] if db_settings['mongo_connect_params'] else ''
 
-    if _mongo_user:
-        
-        _conn_uri = 'mongodb://{user}:{pswd}@{host}:{port}{connect_params}'.format(user=_mongo_user, pswd=_mongo_pass, host=_mongo_host, port=_mongo_port, connect_params=_mongo_connect_params)
-        
-        ilog.info("CONNECTION_URI: {}".format(_conn_uri))
-
-        _client = motor.motor_tornado.MotorClient(_mongo_host, _mongo_port)
+    if _conn_uri:
+        _client = motor.motor_tornado.MotorClient(_conn_uri)
     else:
-        _client = motor.motor_tornado.MotorClient(_mongo_host, _mongo_port).dbtickets
+        _client = motor.motor_tornado.MotorClient(_mongo_host, _mongo_port)
 
     _db = _client.dbtickets
 
     _db_conn_sts = True
     ilog.info("[Mongo] - DB connection has established!")
+
 except Exception as e:
     _db = None
     _db_conn_sts = False
